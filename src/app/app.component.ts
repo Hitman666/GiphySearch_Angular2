0e31c530549d943830dab0fe05ec5c9de039c532
Nikola <nikola.breznjak@gmail.com>
2016-07-11 09:12:59 +0200

    Finished project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+    moduleId: module.id,
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
-}
+    title = 'Welcome to GiphySearch';
+    link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+    http: Http;
+    giphies = [];
+
+    constructor(http: Http) {
+        this.http = http;
+    }
+
+    performSearch(searchTerm: HTMLInputElement): void {
+        var apiLink = this.link + searchTerm.value;
+
+        this.http.request(apiLink)
+            .subscribe((res: Response) => {
+                  this.giphies = res.json().data;
+                  console.log(this.giphies);
+            });
+    }
+};
